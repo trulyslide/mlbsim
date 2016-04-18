9dafbc2711a0e847349cc9b3568ade3480cc9159
Joe Adkins <joeadk@gmail.com>
2016-04-18 14:58:17 -0400

    Update util_gamestats.py

--- util_gamestats.py
+++ util_gamestats.py
@@ -1,13 +1,17 @@
 from datetime import datetime
 import mongo_connect
-
-today = int(datetime.today().strftime("%Y%m%d"))
+import json
+import urllib2
 
 db = mongo_connect.connect()
+today = int(datetime.today().strftime("%Y%m%d"))
+
 dates = db.game_dates.find({"date": {"$gt": 20160101,"$lt": today}})
 for date in dates:
 	dateStr =  str(date['date'])
 	year = dateStr[:4]
 	month = dateStr[4:6]
 	day = dateStr[-2:]
-	print year+month+day
+	url = "http://gd2.mlb.com/components/game/mlb/year_" + year + "/month_" + month + "/day_" + day + "/miniscoreboard.json"
+	print url
+	#json.load(urllib2.urlopen("url"))
