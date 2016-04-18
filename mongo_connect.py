fb2d9e7a6e5b7fa8619b2fd184298b1be4ea46f2
Joe Adkins <joeadk@gmail.com>
2016-04-18 12:48:41 -0400

    Update mongo_connect.py

--- mongo_connect.py
+++ mongo_connect.py
@@ -1,11 +1,13 @@
 from pymongo import MongoClient
 from ConfigParser import SafeConfigParser
 
-parser = SafeConfigParser()
-parser.read('settings.ini')
-username =  parser.get('mongodb', 'username')
-password =  parser.get('mongodb', 'password')
-
-client = MongoClient()
-db = client.mlb
-db.authenticate(username, password)
+def connect:
+  parser = SafeConfigParser()
+  parser.read('settings.ini')
+  username =  parser.get('mongodb', 'username')
+  password =  parser.get('mongodb', 'password')
+  
+  client = MongoClient()
+  db = client.mlb
+  db.authenticate(username, password)
+  return db
