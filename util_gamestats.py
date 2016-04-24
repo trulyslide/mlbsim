bcc0cfac8e9d131ea00afaf1ae8d6e32114d1351
Joe Adkins <joe@bardc.com>
2016-04-24 10:26:10 -0400

    stat retrieval

--- util_gamestats.py
+++ util_gamestats.py
@@ -7,20 +7,23 @@ import util_gamestats_game
 db = mongo_connect.connect()
 today = int(datetime.today().strftime("%Y%m%d"))
 
-dates = db.game_dates.find({"date": {"$gt": 20160101,"$lt": today}})
+dates = db.game_dates.find({"date": {"$gt": 20160401,"$lt": today}})
 for date in dates:
 	dateStr =  str(date['date'])
 	year = dateStr[:4]
 	month = dateStr[4:6]
 	day = dateStr[-2:]
+	formattedDate = year + "_" + month + "_" + day
 	url = "http://gd2.mlb.com/components/game/mlb/year_" + year + "/month_" + month + "/day_" + day + "/miniscoreboard.json"
 	scoreboard = json.load(urllib2.urlopen(url))
 	games = scoreboard['data']['games']['game']
 	for game in games:
+		game_type = game['game_type']
+		status = game['status']
 		gameday_link = game['game_data_directory']
 		gameID = game['gameday_link']
-		gameURL = "http://gd2.mlb.com" + gameday_link + "/inning/inning_all.xml"
-		gameData = urllib2.urlopen(gameURL)
-		util_gamestats_game.parseGameData(gameData,gameID)
-		break
-	break
+		print gameID
+		if(game_type == "R" and status != "Postponed"):
+			gameURL = "http://gd2.mlb.com" + gameday_link + "/inning/inning_all.xml"
+			gameData = urllib2.urlopen(gameURL)
+			util_gamestats_game.parseGameData(gameData,gameID,formattedDate)
--- util_gamestats_game.py
+++ util_gamestats_game.py
@@ -1,53 +1,90 @@
 import xml.etree.ElementTree as ET
+import mongo_connect
 
-def parseGameData(gameData,gameID):
+def parseGameData(gameData,gameID,formattedDate):
+	db = mongo_connect.connect()
 	tree = ET.parse(gameData)
 	root = tree.getroot()
 	for inning in root.iter('inning'):
-		inningNum = inning.atrib['num']
+		inningNum = inning.attrib['num']
 		for side in inning:
 			inningSide = side.tag
 			for atbat in side:
 				if atbat.tag == "atbat": 
-					num = atbat.atrib['num']
-					b = atbat.atrib['b']
-					s = atbat.atrib['s']
-					o = atbat.atrib['o']
-					start_tfs = atbat.atrib['start_tfs']
-					start_tfs_zulu = atbat.atrib['start_tfs_zulu']
-					batter = atbat.atrib['batter']
-					stand = atbat.atrib['stand']
-					pitcher = atbat.atrib['pitcher']
-					p_throws = atbat.atrib['p_throws']
-					des = atbat.atrib['des']
-					event_num = atbat.atrib['event_num']
-					event = atbat.atrib['event']
-					play_guid = atbat.atrib['play_guid']
-					home_team_runs = atbat.atrib['home_team_runs']
-					away_team_runs = atbat.atrib['away_team_runs']
-
-					result = dbMongo.batter_PA.insert_one(
+					num = atbat.attrib['num']
+					b = atbat.attrib['b']
+					s = atbat.attrib['s']
+					o = atbat.attrib['o']
+					start_tfs = atbat.attrib['start_tfs']
+					start_tfs_zulu = atbat.attrib['start_tfs_zulu']
+					batter = atbat.attrib['batter']
+					stand = atbat.attrib['stand']
+					pitcher = atbat.attrib['pitcher']
+					p_throws = atbat.attrib['p_throws']
+					des = atbat.attrib['des']
+					event_num = atbat.attrib['event_num']
+					event = atbat.attrib['event']
+					if 'play_guid' in atbat.attrib:
+						play_guid = atbat.attrib['play_guid']
+					else:
+						play_guid = ""
+					home_team_runs = atbat.attrib['home_team_runs']
+					away_team_runs = atbat.attrib['away_team_runs']
+					print event_num
+					result = db.batter_PA.update(
+    				{
+    					"gameday_link" : gameID,
+    					"event_num": event_num
+    				},
     				{
         				"playerID" : batter,
         				"pitcherID" : pitcher,
         				"inning" : inningNum,
         				"gamelink_num" : gameID,
         				"play_guid" : play_guid,
-        				"date" : xxx,
-        				"p_throws" : xxx,
-        				"event" : xxx,
-        				"score" : xxx,
-        				"away_team_runs" : xxx,
-        				"event_num" : xxx,
-        				"stand" : xxx,
-        				"start_tfs" : xxx,
-        				"atbat_des" : xxx,
+        				"date" : formattedDate,
+        				"p_throws" : p_throws,
+        				"event" : event,
+        				"away_team_runs" : away_team_runs,
+        				"event_num" : event_num,
+        				"stand" : stand,
+        				"start_tfs" : start_tfs,
+        				"atbat_des" : des,
         				"inning_side" : inningSide,
-        				"b" : xxx,
-        				"home_team_runs" : xxx,
-        				"gameday_link" : xxx,
-        				"o" : xxx,
-        				"s" : xxx,
-        				"start_tfs_zulu" : xxx
-    				})
+        				"b" : b,
+        				"home_team_runs" : home_team_runs,
+        				"gameday_link" : gameID,
+        				"o" : o,
+        				"s" : s,
+        				"start_tfs_zulu" : start_tfs_zulu
+    				},
+    				upsert=True)
+    				result = db.pitcher_PA.update(
+    				{
+    					"gameday_link" : gameID,
+    					"event_num": event_num
+    				},
+    				{
+        				"playerID" : pitcher,
+        				"batterID" : batter,
+        				"inning" : inningNum,
+        				"gamelink_num" : gameID,
+        				"play_guid" : play_guid,
+        				"date" : formattedDate,
+        				"p_throws" : p_throws,
+        				"event" : event,
+        				"away_team_runs" : away_team_runs,
+        				"event_num" : event_num,
+        				"stand" : stand,
+        				"start_tfs" : start_tfs,
+        				"atbat_des" : des,
+        				"inning_side" : inningSide,
+        				"b" : b,
+        				"home_team_runs" : home_team_runs,
+        				"gameday_link" : gameID,
+        				"o" : o,
+        				"s" : s,
+        				"start_tfs_zulu" : start_tfs_zulu
+    				},
+    				upsert=True)
 	return "True"
