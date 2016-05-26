c35660a981244b7bfd434a7326fd8cd08a8b0539
Joe Adkins <joe@bardc.com>
2016-05-25 22:35:01 -0400

    test

--- util_gamestats_game.py
+++ util_gamestats_game.py
@@ -8,12 +8,9 @@ def parseGameData(gameData,gameID,formattedDate):
 	event_num = ""
 	for inning in root.iter('inning'):
 		inningNum = inning.attrib['num']
-		print inningNum
 		for side in inning:
 			inningSide = side.tag
-			print inningSide
 			for atbat in side:
-				print atbat.tag
 				if atbat.tag == "atbat": 
 					num = atbat.attrib['num']
 					print num
