b4bc464646d05a7630b60b531dccb1161b186c68
Joe Adkins <joeadk@gmail.com>
2016-06-16 14:44:55 -0400

    Update teams.py

--- teams.py
+++ teams.py
@@ -2,242 +2,153 @@ import mongo_connect
 db = mongo_connect.connect()
 
 db.teams.remove({})
-db.teams.update(
-	{
-    "team": "atl"
-    },
+
+db.teams.insert(
     {
 	"team": "atl",
     "webID": "atl"
     })
-db.teams.update(
-	{
-    "team": "mia"
-    },
+db.teams.insert(
     {
 	"team": "mia",
     "webID": "mia"
     })
-db.teams.update(
-	{
-    "team": "phi"
-    },
+db.teams.insert(
     {
 	"team": "phi",
     "webID": "phi"
     })
-db.teams.update(
-	{
-    "team": "nym"
-    },
+db.teams.insert(
     {
 	"team": "nyn",
     "webID": "nym"
     })
-db.teams.update(
-	{
-    "team": "was"
-    },
+db.teams.insert(
     {
 	"team": "was",
     "webID": "was"
     })
-db.teams.update(
-	{
-    "team": "chc"
-    },
+db.teams.insert(
     {
 	"team": "chn",
     "webID": "chc"
     })
-db.teams.update(
-	{
-    "team": "cin"
-    },
+db.teams.insert(
     {
 	"team": "cin",
     "webID": "cin"
     })
-db.teams.update(
-	{
-    "team": "pit"
-    },
+db.teams.insert(
     {
 	"team": "pit",
     "webID": "pit"
     })
-db.teams.update(
-	{
-    "team": "mil"
-    },
+db.teams.insert(
     {
 	"team": "mil",
     "webID": "mil"
     })
-db.teams.update(
-	{
-    "team": "stl"
-    },
+db.teams.insert(
     {
 	"team": "sln",
     "webID": "stl"
     })
-db.teams.update(
-	{
-    "team": "la"
-    },
+db.teams.insert(
     {
 	"team": "lan",
     "webID": "la"
     })
-db.teams.update(
-	{
-    "team": "sf"
-    },
+db.teams.insert(
     {
 	"team": "sfn",
     "webID": "sf"
     })
-db.teams.update(
-	{
-    "team": "sd"
-    },
+db.teams.insert(
     {
 	"team": "sdn",
     "webID": "sd"
     })
-db.teams.update(
-	{
-    "team": "col"
-    },
+db.teams.insert(
     {
 	"team": "col",
     "webID": "col"
     })
-db.teams.update(
-	{
-    "team": "ari"
-    },
+db.teams.insert(
     {
 	"team": "ari",
     "webID": "ari"
     })
-db.teams.update(
-	{
-    "team": "bal"
-    },
+db.teams.insert(
     {
 	"team": "bal",
     "webID": "bal"
     })
-db.teams.update(
-	{
-    "team": "bos"
-    },
+db.teams.insert(
     {
 	"team": "bos",
     "webID": "bos"
     })
-db.teams.update(
-	{
-    "team": "nyy"
-    },
+db.teams.insert(
     {
 	"team": "nya",
     "webID": "nyy"
     })
-db.teams.update(
-	{
-    "team": "tor"
-    },
+db.teams.insert(
     {
 	"team": "tor",
     "webID": "tor"
     })
-db.teams.update(
-	{
-    "team": "tb"
-    },
+db.teams.insert(
     {
 	"team": "tba",
     "webID": "tb"
     })
-db.teams.update(
-	{
-    "team": "cle"
-    },
+db.teams.insert(
     {
 	"team": "cle",
     "webID": "cle"
     })
-db.teams.update(
-	{
-    "team": "det"
-    },
+db.teams.insert(
     {
 	"team": "det",
     "webID": "det"
     })
-db.teams.update(
-	{
-    "team": "min"
-    },
+db.teams.insert(
     {
 	"team": "min",
     "webID": "min"
     })
-db.teams.update(
-	{
-    "team": "chw"
-    },
+db.teams.insert(
     {
 	"team": "cha",
     "webID": "chw"
     })
-db.teams.update(
-	{
-    "team": "kc"
-    },
+db.teams.insert(
     {
 	"team": "kca",
     "webID": "kc"
     })
-db.teams.update(
-	{
-    "team": "ana"
-    },
+db.teams.insert(
     {
 	"team": "ana",
     "webID": "ana"
     })
-db.teams.update(
-	{
-    "team": "tex"
-    },
+db.teams.insert(
     {
 	"team": "tex",
     "webID": "tex"
     })
-db.teams.update(
-	{
-    "team": "oak"
-    },
+db.teams.insert(
     {
 	"team": "oak",
     "webID": "oak"
     })
-db.teams.update(
-	{
-    "team": "sea"
-    },
+db.teams.insert(
     {
 	"team": "sea",
     "webID": "sea"
     })
-db.teams.update(
-	{
-    "team": "hou"
-    },
+db.teams.insert(
     {
 	"team": "hou",
     "webID": "hou"
