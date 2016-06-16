e13bb53e2b6f7d411cf9f741d66648b4ca7fa029
Joe Adkins <joeadk@gmail.com>
2016-06-16 11:41:18 -0400

    Update daily_player_daily.py

--- daily_player_daily.py
+++ daily_player_daily.py
@@ -117,7 +117,7 @@ def player_daily():
 			lastDateUpdated = data['dateUpdated']
 			print lastDateUpdated
 	else:
-		lastDateUpdated = 20160801
+		lastDateUpdated = 20150801
 	#get rows where date > lastDateUpdated
 	batterPAs = db.batter_PA.find( { "date": {"$gt": lastDateUpdated }} ).sort([("date", 1),("playerID", 1)])
 	pitcherPAs = db.pitcher_PA.find( { "date": {"$gt": lastDateUpdated }} ).sort([("date", 1),("playerID", 1)])
