9b83d396baae56d777b7af8f4e69f0c01a512c92
Joe Adkins <joeadk@gmail.com>
2016-06-16 15:20:59 -0400

    Update util_matrix.py

--- util_matrix.py
+++ util_matrix.py
@@ -30,6 +30,8 @@ for game in games:
 	print home_team + " - " + away_team
 	homeRoster = db.rosters.find( { "team": home_team })
 	awayRoster = db.rosters.find( { "team": away_team })
+	print homeRoster.count()
+	print awayRoster.count()
 	for player in homeRoster:
 		#print player['name'] + " - " + player['pos']
 		playerID = str(player['playerID'])
