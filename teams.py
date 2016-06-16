28e884b4ff386b4ad98daad8f0b813dbe4c2e03d
Joe Adkins <joeadk@gmail.com>
2016-06-16 14:24:04 -0400

    Create teams.py

---
+++ teams.py
@@ -0,0 +1,242 @@
+import mongo_connect
+
+db.teams.update(
+	{
+    "team": "atl"
+    },
+    {
+	"team": "atl",
+    "webID": "atl"
+    })
+db.teams.update(
+	{
+    "team": "mia"
+    },
+    {
+	"team": "mia",
+    "webID": "mia"
+    })
+db.teams.update(
+	{
+    "team": "phi"
+    },
+    {
+	"team": "phi",
+    "webID": "phi"
+    })
+db.teams.update(
+	{
+    "team": "nym"
+    },
+    {
+	"team": "nym",
+    "webID": "nyn"
+    })
+db.teams.update(
+	{
+    "team": "was"
+    },
+    {
+	"team": "was",
+    "webID": "was"
+    })
+db.teams.update(
+	{
+    "team": "chc"
+    },
+    {
+	"team": "chc",
+    "webID": "chn"
+    })
+db.teams.update(
+	{
+    "team": "cin"
+    },
+    {
+	"team": "cin",
+    "webID": "cin"
+    })
+db.teams.update(
+	{
+    "team": "pit"
+    },
+    {
+	"team": "pit",
+    "webID": "pit"
+    })
+db.teams.update(
+	{
+    "team": "mil"
+    },
+    {
+	"team": "mil",
+    "webID": "mil"
+    })
+db.teams.update(
+	{
+    "team": "stl"
+    },
+    {
+	"team": "stl",
+    "webID": "sln"
+    })
+db.teams.update(
+	{
+    "team": "la"
+    },
+    {
+	"team": "la",
+    "webID": "lan"
+    })
+db.teams.update(
+	{
+    "team": "sf"
+    },
+    {
+	"team": "sf",
+    "webID": "sfn"
+    })
+db.teams.update(
+	{
+    "team": "sd"
+    },
+    {
+	"team": "sd",
+    "webID": "sdn"
+    })
+db.teams.update(
+	{
+    "team": "col"
+    },
+    {
+	"team": "col",
+    "webID": "col"
+    })
+db.teams.update(
+	{
+    "team": "ari"
+    },
+    {
+	"team": "ari",
+    "webID": "ari"
+    })
+db.teams.update(
+	{
+    "team": "bal"
+    },
+    {
+	"team": "bal",
+    "webID": "bal"
+    })
+db.teams.update(
+	{
+    "team": "bos"
+    },
+    {
+	"team": "bos",
+    "webID": "bos"
+    })
+db.teams.update(
+	{
+    "team": "nyy"
+    },
+    {
+	"team": "nyy",
+    "webID": "nya"
+    })
+db.teams.update(
+	{
+    "team": "tor"
+    },
+    {
+	"team": "tor",
+    "webID": "tor"
+    })
+db.teams.update(
+	{
+    "team": "tb"
+    },
+    {
+	"team": "tb",
+    "webID": "tba"
+    })
+db.teams.update(
+	{
+    "team": "cle"
+    },
+    {
+	"team": "cle",
+    "webID": "cle"
+    })
+db.teams.update(
+	{
+    "team": "det"
+    },
+    {
+	"team": "det",
+    "webID": "det"
+    })
+db.teams.update(
+	{
+    "team": "min"
+    },
+    {
+	"team": "min",
+    "webID": "min"
+    })
+db.teams.update(
+	{
+    "team": "chw"
+    },
+    {
+	"team": "chw",
+    "webID": "cha"
+    })
+db.teams.update(
+	{
+    "team": "kc"
+    },
+    {
+	"team": "kc",
+    "webID": "kca"
+    })
+db.teams.update(
+	{
+    "team": "ana"
+    },
+    {
+	"team": "ana",
+    "webID": "ana"
+    })
+db.teams.update(
+	{
+    "team": "tex"
+    },
+    {
+	"team": "tex",
+    "webID": "tex"
+    })
+db.teams.update(
+	{
+    "team": "oak"
+    },
+    {
+	"team": "oak",
+    "webID": "oak"
+    })
+db.teams.update(
+	{
+    "team": "sea"
+    },
+    {
+	"team": "sea",
+    "webID": "sea"
+    })
+db.teams.update(
+	{
+    "team": "hou"
+    },
+    {
+	"team": "hou",
+    "webID": "hou"
+    })
