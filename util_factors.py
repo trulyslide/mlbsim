ca2ef126ff24e7f6a506b3e64685059bde4616e9
Joe Adkins <joeadk@gmail.com>
2016-06-21 11:04:32 -0400

    Update util_factors.py

--- util_factors.py
+++ util_factors.py
@@ -1,2 +1,664 @@
 import mongo_connect
 
+db = mongo_connect.connect()
+
+db.factors.insert(
+	{
+    "team" : "ana",
+    "stand" : "L",
+	"HR" : 0.89,
+    "1B" : 1.006,
+    "2B" : 0.902,
+    "3B" : 0.567
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "sfn",
+    "stand" : "L",
+	"HR" : ,
+    "1B" : 0.591,
+    "2B" : 1.036,
+    "3B" : 1.007
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "sln",
+    "stand" : "L",
+	"HR" : 0.864,
+    "1B" : 1.005,
+    "2B" : 0.945,
+    "3B" : 1.2
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "ari",
+    "stand" : "L",
+	"HR" : 1.007,
+    "1B" : 0.99,
+    "2B" : 1.104,
+    "3B" : 1.526
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "nyn",
+    "stand" : "L",
+	"HR" : 1.05,
+    "1B" : 0.837,
+    "2B" : 0.957,
+    "3B" : 0.742
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "phi",
+    "stand" : "L",
+	"HR" : 1.088,
+    "1B" : 0.965,
+    "2B" : 0.926,
+    "3B" : 0.872
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "det",
+    "stand" : "L",
+	"HR" : 0.853,
+    "1B" : 1.002,
+    "2B" : 0.797,
+    "3B" : 1.092
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "col",
+    "stand" : "L",
+	"HR" : 1.195,
+    "1B" : 1.209,
+    "2B" : 1.175,
+    "3B" : 1.912
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "lan",
+    "stand" : "L",
+	"HR" : 1.147,
+    "1B" : 0.911,
+    "2B" : 0.978,
+    "3B" : 0.474
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "bos",
+    "stand" : "L",
+	"HR" : 0.701,
+    "1B" : 1.005,
+    "2B" : 1.541,
+    "3B" : 1.459
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "tex",
+    "stand" : "L",
+	"HR" : 1.027,
+    "1B" : 1.083,
+    "2B" : 0.861,
+    "3B" : 1.177
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "cin",
+    "stand" : "L",
+	"HR" : 1.252,
+    "1B" : 0.962,
+    "2B" : 0.892,
+    "3B" : 0.818
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "kca",
+    "stand" : "L",
+	"HR" : 0.882,
+    "1B" : 0.977,
+    "2B" : 1.083,
+    "3B" : 1.324
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "mia",
+    "stand" : "L",
+	"HR" : 0.635,
+    "1B" : 1.029,
+    "2B" : 1.018,
+    "3B" : 1.2
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "mil",
+    "stand" : "L",
+	"HR" : 1.362,
+    "1B" : 1.119,
+    "2B" : 1.053,
+    "3B" : 0.833
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "hou",
+    "stand" : "L",
+	"HR" : 1.21,
+    "1B" : 0.958,
+    "2B" : 0.872,
+    "3B" : 1.276
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "was",
+    "stand" : "L",
+	"HR" : 0.816,
+    "1B" : 1.108,
+    "2B" : 1.004,
+    "3B" : 0.59
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "oak",
+    "stand" : "L",
+	"HR" : 0.734,
+    "1B" : 0.92,
+    "2B" : 1.119,
+    "3B" : 1.304
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "bal",
+    "stand" : "L",
+	"HR" : 1.427,
+    "1B" : 1.066,
+    "2B" : 0.862,
+    "3B" : 0.556
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "sdn",
+    "stand" : "L",
+	"HR" : 1.128,
+    "1B" : 0.94,
+    "2B" : 0.901,
+    "3B" : 0.918
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "pit",
+    "stand" : "L",
+	"HR" : 0.938,
+    "1B" : 0.965,
+    "2B" : 0.906,
+    "3B" : 0.6
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "cle",
+    "stand" : "L",
+	"HR" : 1.028,
+    "1B" : 1.03,
+    "2B" : 1.168,
+    "3B" : 0.677
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "tor",
+    "stand" : "L",
+	"HR" : 1.105,
+    "1B" : 0.898,
+    "2B" : 1.152,
+    "3B" : 1.062
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "sea",
+    "stand" : "L",
+	"HR" : 1.005,
+    "1B" : 0.995,
+    "2B" : 0.929,
+    "3B" : 0.625
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "min",
+    "stand" : "L",
+	"HR" : 0.962,
+    "1B" : 1.044,
+    "2B" : 1.01,
+    "3B" : 1.2
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "tba",
+    "stand" : "L",
+	"HR" : 1.114,
+    "1B" : 0.951,
+    "2B" : 0.984,
+    "3B" : 0.923
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "atl",
+    "stand" : "L",
+	"HR" : 0.868,
+    "1B" : 0.986,
+    "2B" : 0.997,
+    "3B" : 0.706
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "cha",
+    "stand" : "L",
+	"HR" : 1.056,
+    "1B" : 0.909,
+    "2B" : 0.919,
+    "3B" : 1.242
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "chn",
+    "stand" : "L",
+	"HR" : 0.966,
+    "1B" : 1.106,
+    "2B" : 1.026,
+    "3B" : 0.944
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "nya",
+    "stand" : "L",
+	"HR" : 1.298,
+    "1B" : 1.046,
+    "2B" : 0.997,
+    "3B" : 0.62
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "ana",
+    "stand" : "R",
+    "HR" : 0.892,
+    "1B" : 1.047,
+    "2B" : 0.943,
+    "3B" : 0.929
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "sfn",
+    "stand" : "R",
+    "HR" : 0.753,
+    "1B" : 0.917,
+    "2B" : 0.903,
+    "3B" : 1.641
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "sln",
+    "stand" : "R",
+    "HR" : 0.919,
+    "1B" : 1.023,
+    "2B" : 1.067,
+    "3B" : 1
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "ari",
+    "stand" : "R",
+    "HR" : 0.932,
+    "1B" : 0.955,
+    "2B" : 1.123,
+    "3B" : 1.944
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "nym",
+    "stand" : "R",
+    "HR" : 1,
+    "1B" : 0.931,
+    "2B" : 0.93,
+    "3B" : 0.675
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "phi",
+    "stand" : "R",
+    "HR" : 1.418,
+    "1B" : 0.945,
+    "2B" : 0.971,
+    "3B" : 0.791
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "det",
+    "stand" : "R",
+    "HR" : 0.94,
+    "1B" : 1.016,
+    "2B" : 1.004,
+    "3B" : 1.809
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "col",
+    "stand" : "R",
+    "HR" : 1.338,
+    "1B" : 1.298,
+    "2B" : 1.255,
+    "3B" : 1.705
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "lan",
+    "stand" : "R",
+    "HR" : 1,
+    "1B" : 0.901,
+    "2B" : 1.032,
+    "3B" : 0.453
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "bos",
+    "stand" : "R",
+    "HR" : 0.996,
+    "1B" : 1.039,
+    "2B" : 1.129,
+    "3B" : 0.667
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "tex",
+    "stand" : "R",
+    "HR" : 0.929,
+    "1B" : 1.025,
+    "2B" : 0.9,
+    "3B" : 1.089
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "cin",
+    "stand" : "R",
+    "HR" : 1.19,
+    "1B" : 1.017,
+    "2B" : 1.083,
+    "3B" : 0.562
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "kca",
+    "stand" : "R",
+    "HR" : 0.82,
+    "1B" : 1.038,
+    "2B" : 1.07,
+    "3B" : 1.333
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "mia",
+    "stand" : "R",
+    "HR" : 0.755,
+    "1B" : 0.97,
+    "2B" : 1.042,
+    "3B" : 1.667
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "mil",
+    "stand" : "R",
+    "HR" : 1.191,
+    "1B" : 0.942,
+    "2B" : 0.947,
+    "3B" : 1
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "hou",
+    "stand" : "R",
+    "HR" : 1.103,
+    "1B" : 0.968,
+    "2B" : 1.021,
+    "3B" : 1.581
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "was",
+    "stand" : "R",
+    "HR" : 0.881,
+    "1B" : 1.135,
+    "2B" : 1.122,
+    "3B" : 0.327
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "oak",
+    "stand" : "R",
+    "HR" : 0.911,
+    "1B" : 0.996,
+    "2B" : 1.075,
+    "3B" : 1.034
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "bal",
+    "stand" : "R",
+    "HR" : 1.068,
+    "1B" : 1.022,
+    "2B" : 0.933,
+    "3B" : 0.667
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "sdn",
+    "stand" : "R",
+    "HR" : 0.882,
+    "1B" : 0.94,
+    "2B" : 0.795,
+    "3B" : 0.974
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "pit",
+    "stand" : "R",
+    "HR" : 0.744,
+    "1B" : 1.078,
+    "2B" : 0.959,
+    "3B" : 0.907
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "cle",
+    "stand" : "R",
+    "HR" : 1.027,
+    "1B" : 0.968,
+    "2B" : 1.223,
+    "3B" : 0.639
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "tor",
+    "stand" : "R",
+    "HR" : 1.197,
+    "1B" : 0.902,
+    "2B" : 1.198,
+    "3B" : 0.9
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "sea",
+    "stand" : "R",
+    "HR" : 0.871,
+    "1B" : 0.944,
+    "2B" : 0.862,
+    "3B" : 0.667
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "min",
+    "stand" : "R",
+    "HR" : 1.008,
+    "1B" : 1.052,
+    "2B" : 1.173,
+    "3B" : 0.805
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "tba",
+    "stand" : "R",
+    "HR" : 0.841,
+    "1B" : 1.011,
+    "2B" : 0.844,
+    "3B" : 0.893
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "atl",
+    "stand" : "R",
+    "HR" : 0.94,
+    "1B" : 0.99,
+    "2B" : 0.833,
+    "3B" : 0.939
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "cha",
+    "stand" : "R",
+    "HR" : 1.16,
+    "1B" : 1.016,
+    "2B" : 0.895,
+    "3B" : 0.569
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "chn",
+    "stand" : "R",
+    "HR" : 1.195,
+    "1B" : 0.927,
+    "2B" : 0.937,
+    "3B" : 1.206
+	}
+)
+
+db.factors.insert(
+	{
+    "team" : "nya",
+    "stand" : "R",
+    "HR" : 1.239,
+    "1B" : 0.958,
+    "2B" : 0.804,
+    "3B" : 0.696
+	}
+)
+
