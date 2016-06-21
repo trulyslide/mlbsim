6f6d207411dd98d50d9a9a0fcca590b09afdd3fc
Joe Adkins <joeadk@gmail.com>
2016-06-21 11:21:08 -0400

    Create util_test.py

---
+++ util_test.py
@@ -0,0 +1,17 @@
+import mongo_connect
+db = mongo_connect.connect()
+
+batterPAs = db.batter_PA.find( { "date": {"$gt": 20150101 }} ).sort([("date", 1),("playerID", 1)])
+for pa in batterPAs:
+  stand = pa['stand']
+  gameID = pa['gamelink_num']
+  parts = gameID.split("_")
+  park = parts[5][:3]
+  factors = db.factors.find_one({"team": park, "stand": stand})
+  facHR = factors['HR']
+  fac1B = factors['1B']
+  fac2B = factors['2B']
+  fac3B = factors['3B']
+      
+  print gameID + park + stand + str(facHR)
+  break
