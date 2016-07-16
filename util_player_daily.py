bd4944c3de9af3feb422e0805e9c1964fce29a1a
Joe Adkins <joe@bardc.com>
2016-07-16 07:12:34 -0400

    fix

--- util_player_daily.py
+++ util_player_daily.py
@@ -137,8 +137,8 @@ for data in dataStatus:
 	print lastDateUpdated
 
 #get rows where date > lastDateUpdated
-batterPAs = db.batter_PA.find( { "date": {"$gt": lastDateUpdated }} ).sort([("date", 1),("playerID", 1),("throws", 1)])
-pitcherPAs = db.pitcher_PA.find( { "date": {"$gt": lastDateUpdated }} ).sort([("date", 1),("playerID", 1),("stand", 1)])
+batterPAs = db.batter_PA.find( { "date": {"$gt": '20160401' }} ).sort([("date", 1),("playerID", 1),("throws", 1)])
+pitcherPAs = db.pitcher_PA.find( { "date": {"$gt": '20160401' }} ).sort([("date", 1),("playerID", 1),("stand", 1)])
 print batterPAs.count()
 print pitcherPAs.count()
 lastPADate = ""
